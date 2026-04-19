7e1141cb1274329315947868c1f233d2ea9470f8
asaeppel4231 <asaeppel4231@gmx.de>
2026-04-19 15:45:05 +0200

    Add generated Doxygen documentation @ A1

---
+++ docs/html/search/defines_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['yafalse_0',['YAFALSE',['../yabool_8h.html#ab10a73d19541f8e2dde092fb72b3a26d',1,'yabool.h']]],
+  ['yatrue_1',['YATRUE',['../yabool_8h.html#ad81dc25df0264ef62424f3b9040eb5db',1,'yabool.h']]]
+];
---
+++ docs/html/search/typedefs_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['yabool_0',['yabool',['../yabool_8h.html#ab646db4af8c03ad0c9488c78248b2213',1,'yabool.h']]]
+];
