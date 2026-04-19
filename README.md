fab4597dca4162be22bf344ea9b579ebc1612b18
asaeppel4231 <asaeppel4231@gmx.de>
2026-04-19 15:35:08 +0200

    Add doxygen documentation and doxygen settings file, update README @ A1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # yabool
+
+A simple header only bool library for the ya ecosystem.
--- yabool.h
+++ yabool.h
@@ -1,39 +1,81 @@
-/*
-This file is a drop-in-replacement of stdbool.h to make 
-h2ot3 ANSI C compatible
+/** @file yabool.h
+  * @author asaeppel4231
+  * @version 0.1
+  * @brief Drop-in replacement of stdbool.h to make my programs and other ya- libraries ANSI C compatible.
+  *
+  * This header defines the `yabool` type and related helper functions.
+  * It is designed to be minimal, portable, and usable in ANSI C environments where stdbool.h is not available.
 */
-#ifndef MBOOL_H
-#define MBOOL_H
 
-#include <stddef.h>
+#ifndef YABOOL_H
+#define YABOOL_H
 
+#include <stddef.h> //!< Just included for the NULL pointer
+
+/**
+ * @typedef yabool
+ * @brief Boolean type (optionally) used by the ya-framework.
+ *
+ * This is an unsigned char that represents boolean values in ANSI C.
+ * Use YATRUE and YAFALSE instead of 1 and 0 for clarity.
+ */
 typedef unsigned char yabool;
 
-#define MTRUE 1
-#define MFALSE 0
+/**
+ * @def YATRUE
+ * @brief Boolean true value for yabool.
+ */
+#define YATRUE 1
 
+/**
+ * @def YAFALSE
+ * @brief Boolean false value for yabool.
+ */
+#define YAFALSE 0
+
+/**
+ * @brief Checks whether a yabool value is false.
+ * @param boolean The value to check.
+ * @return 1 if false, 0 otherwise.
+ */
 static inline int yabool_is_false(yabool boolean){
-    return boolean == MFALSE;
+    return boolean == YAFALSE;
 }
 
+/**
+ * @brief Checks whether a yabool value is true.
+ * @param boolean The value to check.
+ * @return 1 if true, 0 otherwise.
+ */
 static inline int yabool_is_true(yabool boolean){
-    return boolean == MTRUE;
+    return boolean == YATRUE;
 }
 
+/**
+ * @brief Sets a yabool variable to false.
+ * @param boolean Pointer to the variable to modify.
+ * @return YATRUE on success, YAFALSE if the pointer is NULL.
+ */
 static inline int yabool_set_false(yabool* boolean){
     if(boolean == NULL){
-        return MFALSE;
+        return YAFALSE;
     }
-    *boolean = MFALSE;
-    return MTRUE; 
+    *boolean = YAFALSE;
+    return YATRUE;
 }
 
+
+/**
+ * @brief Sets a yabool variable to true.
+ * @param boolean Pointer to the variable to modify.
+ * @return YATRUE on success, YAFALSE if the pointer is NULL.
+ */
 static inline int yabool_set_true(yabool* boolean){
     if(boolean == NULL){
-        return MFALSE;
+        return YAFALSE;
     }
-    *boolean = MTRUE;
-    return MTRUE;
+    *boolean = YATRUE;
+    return YATRUE;
 }
 
 #endif
